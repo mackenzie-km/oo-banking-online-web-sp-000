41f56228387ee11d771707b43a7fba3a6507c618
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 19:36:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ def reverse_transfer
   if @status == "complete" && valid?
     @sender.balance += @amount 
     @receiver.balance -= @amount
+    @status = "reversed"
   else   
     end 
 end 
@@ -28,7 +29,7 @@ def execute_transaction
   if valid? == true 
     @sender.balance -= @amount 
     @receiver.balance += @amount 
-    @status = "reversed"
+    @status = "complete"
   else 
     @status = "rejected"
     "Transaction rejected. Please check your account balance."
