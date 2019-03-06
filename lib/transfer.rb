ec3fee57ccce7160c7edafc7d1dffc654f74a835
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-06 19:23:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,10 @@
 class Transfer
+  attr_accessor :pending, :amount 
   attr_reader :sender, :receiver
 def initialize(sender, receiver, amount)
   @sender = sender
   @receiver = receiver
+  @amount = amount 
+  @status = "pending"
 end 
 end
